e39e56d0808fa84a893c8b7d514920a236f96a6e
VaibhavRai24 <vairaibhav922@gmail.com>
2025-03-23 11:39:53 +0530

    STARTED THE MODEL PREPARATION IN WHICH I FIRST HAVE ADDED THE CONFIG AND PARAMS YAML FILES AND DEFINED THE CONFIURATION MANAGER FOR THIS STEP

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 from cnnClassifier.constants import *
 import os
 from cnnClassifier.utils.common import read_yaml, create_directories, save_bin, save_json
-from cnnClassifier.entity.config_entity import (DataIngestionConfigEntity)
+from cnnClassifier.entity.config_entity import (DataIngestionConfigEntity,
+                                                PrepareBaseModelConfigEntity)
 
 class ConfigurationManager:
     def __init__(self, config_file_path= CONFIG_FILE_PATH, params_filepath = PARAMS_FILE_PATH ):
@@ -26,4 +27,20 @@ class ConfigurationManager:
         return data_ingestion_config
     
     
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfigEntity:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        prepare_base_model_config = PrepareBaseModelConfigEntity(
+            root_dir= Path(config.root_dir),
+            base_model_path= Path(config.base_model_path),
+            updated_base_model_path= Path(config.updated_base_model_path),
+            params_image_size= self.params.IMAGE_SIZE,
+            params_learning_rate= self.params.LEARNING_RATE,
+            params_include_top= self.params.INCLUDE_TOP,
+            params_weights= self.params.WEIGTHS,
+            params_classes= self.params.CLASSES
+        )
+
+        
     
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -8,3 +8,15 @@ class DataIngestionConfigEntity:
     local_data_file_path: Path
     unzip_dir: Path
     
+    
+    
+@dataclass(frozen= True)
+class PrepareBaseModelConfigEntity:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_leanring_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
